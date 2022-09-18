b51ae0aa23bbe6689daf44289fa7600560372006
Tim Morgan <git@timothymorgan.info>
2022-09-18 12:32:12 -0700

    Textile -> Markdown

--- lib/ip_address_validator.rb
+++ lib/ip_address_validator.rb
@@ -1,17 +1,20 @@
 require 'localized_each_validator'
 require 'ipaddr'
 
-# Validates IPv4 and IPv6 addresses. Uses the @invalid_ip@ error message key.
+# Validates IPv4 and IPv6 addresses. Uses the `invalid_ip` error message key.
 #
 # @example
 #   validates :last_login_ip, ip_address: true
 #
-# h2. Options
+# Options
+# -------
 #
-# | @:ipv4_only@ | If @true@, IPv6 addresses are considered invalid. |
-# | @:ipv6_only@ | If @true@, IPv4 addresses are considered invalid. |
-# | @:message@ | A custom message to use if the IP is invalid. |
-# | @:allow_nil@ | If true, @nil@ values are allowed. |
+# |              |                                                   |
+# |:-------------|:--------------------------------------------------|
+# | `:ipv4_only` | If `true`, IPv6 addresses are considered invalid. |
+# | `:ipv6_only` | If `true`, IPv4 addresses are considered invalid. |
+# | `:message`   | A custom message to use if the IP is invalid.     |
+# | `:allow_nil` | If `true`, `nil` values are allowed.              |
 
 class IpAddressValidator < LocalizedEachValidator
   error_key :invalid_ip
@@ -24,10 +27,10 @@ class IpAddressValidator < LocalizedEachValidator
     rescue ArgumentError
       return false
     end
-    
+
     return false if ip.ipv4? and options[:ipv6_only]
     return false if ip.ipv6? and options[:ipv4_only]
-    
+
     return true
   end
 end
